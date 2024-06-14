90b2ec8f5df695840f86274aef8fc9f13e779c29
nirojneupane5 <nirojneupane5@gmail.com>
2024-06-14 09:56:26 +0545

    underline while hovering under navbar

--- src/components/Navbar/CustomLink.tsx
+++ src/components/Navbar/CustomLink.tsx
@@ -1,13 +1,27 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 type TCustomLinkProp = {
   title: string;
   ahref: string;
+  className?: string;
 };
-const CustomLink = ({ title, ahref }: TCustomLinkProp) => {
+const CustomLink = ({ title, ahref, className }: TCustomLinkProp) => {
+  const pathname = usePathname();
   return (
-    <li className="hover:text-secondaryColor ease-in duration-300">
-      <Link href={ahref}>{title}</Link>
+    <li
+      className={`relative hover:text-secondaryColor ease-in duration-300 group ${className} `}
+    >
+      <Link href={ahref}>
+        {title}
+        <span
+          className={`h-[1px] inline-block bg-secondaryColor absolute -bottom-0.5 left-0  group-hover:w-full transition-[width] ease duration-300
+            ${ahref === pathname ? "w-full" : "w-0"}`}
+        >
+          &nbsp;
+        </span>
+      </Link>
     </li>
   );
 };
